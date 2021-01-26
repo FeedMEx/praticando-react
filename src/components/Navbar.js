102c1f3ca88f044082a3a59dd8aef8254c66f919
feedmex <freddylo_@hotmail.com>
2021-01-26 18:59:37 -0500

    rename title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container">
-                <Link className="navbar-brand" to="/">AEASTORE</Link>
+                <Link className="navbar-brand" to="/">IMGSTORE</Link>
             
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
